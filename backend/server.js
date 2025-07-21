6553d0e71187626b13095b4bd1c0162e76fd5c11
VishalRai7 <raiv23656@gmail.com>
2025-07-21 22:09:15 +0530

    Use dynamic PORT and dotenv for Render deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+/*require('dotenv').config()
 
 
 const app = require('./src/app')
@@ -7,4 +7,9 @@ app.listen(3000,()=>{
 
     console.log('Server is running on http://localhost:3000')
 
-})
+})*/
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
