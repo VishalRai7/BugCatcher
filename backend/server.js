e83caf94cca1f5cd12ba23e22dc68f08a31925cf
VishalRai7 <raiv23656@gmail.com>
2025-07-21 22:27:14 +0530

    Initial setup for Render deployment

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
-/*require('dotenv').config()
+require('dotenv').config()
 
 
 const app = require('./src/app')
 
-app.listen(3000,()=>{
+/*app.listen(3000,()=>{
 
     console.log('Server is running on http://localhost:3000')
 
